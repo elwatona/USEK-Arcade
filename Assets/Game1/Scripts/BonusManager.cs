4a5e81b00faa20b05b8a51e21692fdfad2480f85
NickShogun24 <nicolas.valenzuela.a@zonavirtual.uisek.cl>
2024-11-11 11:20:43 -0300

    Cambios en BonusManager
    Creación de funciones para orientar la creación y objetivo del BonusManager.

--- Assets/Game1/Scripts/BonusManager.cs
+++ Assets/Game1/Scripts/BonusManager.cs
@@ -5,12 +5,14 @@ using TMPro;
 
 public class BonusManager : MonoBehaviour
 {
-    public float x, y;
+    private float x, y;
     public float spd;
-    public TextMeshProUGUI textPoint;
+    public TextMeshProUGUI textTime;
+    public TextMeshProUGUI text;
     public float timer;
     public float time;
     public bool isTimerOn = true;
+    public int puntaje1, puntaje2;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,10 +30,29 @@ public class BonusManager : MonoBehaviour
         //Hacer que los jgadores puedan mover la palanca de manera Horizontal o vertical
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical"); 
+        Vector3 dir = new Vector3(x, y,0);
+        transform.position += dir;
+        // Vector3.right;
+        //Vector3.left;
+        //Vector3.up;
+        //Vector3.down;
     }
     void GanarPuntaje()
     {
         //Los jugadores ganaran 1 punto por cada moviemiento completo realizado (izquierda a derecha || Arriba a Abajo)
+        bool playerPoint1 = false;
+        bool playerPoint2 = false;
+        while(playerPoint1 == true)
+        {
+            puntaje1 = 0;
+            puntaje1++;
+        }
+        while (playerPoint2 == true)
+        {
+            puntaje2 = 0;
+            puntaje2++;
+        }
+        
 
     }
     void TimerTime()
@@ -50,7 +71,26 @@ public class BonusManager : MonoBehaviour
     {
        //Que los jugadores muevan la palanca de manera horizontal o vertical dependiendo de lo que la orientacion pida
         {
+            
+            
+                //changeOrientation cambia la direccion en la que el jugador bombea o mueve la palanca en horizontal o vertical
+                //Vector3.right += puntaje1;
+               
+            
+            
 
         }
+    }
+    void MostrarPuntaje()
+    {
+        //El TextMeshPro debe mostrar el puntaje tanto del player 1 como del player 2;
+    }
+    void EmpezarBonus()
+    {
+
+    }
+    void TerminarBonus()
+    {
+
     }
 }
