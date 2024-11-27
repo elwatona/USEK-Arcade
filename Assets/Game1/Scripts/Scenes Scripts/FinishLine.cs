d6f2811f33e0da3ace4700679ebd03fd8112fcb8
NickShogun24 <nicolas.valenzuela.a@zonavirtual.uisek.cl>
2024-11-27 10:13:29 -0300

    Update Finish

--- Assets/Game1/Scripts/Scenes Scripts/FinishLine.cs
+++ Assets/Game1/Scripts/Scenes Scripts/FinishLine.cs
@@ -9,7 +9,7 @@ public class FinishLine : MonoBehaviour
     {
         if(collision.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            SceneManager.LoadScene("GME1_bonus1");
         }
     }
 }
