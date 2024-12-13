27e5fa525ff13b4e936c5ec6d7dfa366d9af1b59
Bigbadbackstab <83518783+Emiliano-Vasquez-P@users.noreply.github.com>
2024-12-13 14:36:03 -0300

    GameIDGrabberFix

--- Assets/Arcade/Scripts/GameIdGrabber.cs
+++ Assets/Arcade/Scripts/GameIdGrabber.cs
@@ -7,14 +7,19 @@ namespace Arcade
 {
     public class GameIdGrabber : MonoBehaviour
     {
-#if UNITY_EDITOR
         private void Awake()
         {
+            string sceneType = "";
             string gameId = "";
             string scene = SceneManager.GetActiveScene().name;
             gameId = scene.Split("_")[0];
+
+        #if !UNITY_EDITOR
+            Debug.Log("TEST");
+            sceneType = scene.Split("_")[1];
+            if(sceneType == "menu")
+        #endif
             GameData.CurrentGameId = gameId;
         }
     }
-#endif
 }
